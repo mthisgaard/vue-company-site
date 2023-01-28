470b34963e8ad49e25a86a15d71e0378eeb9c26d
Malene Thisgaard <malene.thisgaard@gmail.com>
2023-01-28 14:52:04 +0900

    add animated background and style navbar to be transparent

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,17 +8,17 @@ const showMenu = ref(false);
 
 <template>
   <div>
-    <div class="bg-neutral-50">
+    <div class="">
       <nav class="container px-6 py-6 mx-auto md:flex md:justify-between md:items-center">
         <div class="flex items-center justify-between">
-          <router-link to="/" class=" text-xl font-bold text-gray-800 md:text-2xl hover:text-blue-400">
+          <router-link to="/" class=" text-xl font-bold text-white md:text-2xl hover:text-blue-400">
             Company Name
           </router-link>
           <!-- Mobile menu button -->
           <div @click="showMenu = !showMenu" class="flex md:hidden">
             <button
               type="button"
-              class="opacity-60 hover:opacity-100 focus:outline-none focus:opacity-100"
+              class="text-white opacity-60 hover:opacity-100 focus:outline-none focus:opacity-100"
             >
               <MenuIcon />
             </button>
@@ -27,15 +27,15 @@ const showMenu = ref(false);
         <!-- Mobile Menu open: "flex", Menu closed: "hidden" -->
         <ul
           :class="showMenu ? 'flex' : 'hidden'"
-          class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0"
+          class="flex-col fixed right-6 items-end mt-4 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0"
         >
-          <li class="text-sm font-bold text-gray-800 hover:text-blue-400">
+          <li class="text-sm font-bold text-white hover:text-blue-400">
             <RouterLink to="/">Home</RouterLink>
           </li>
-          <li class="text-sm font-bold text-gray-800 hover:text-blue-400">
+          <li class="text-sm font-bold text-white hover:text-blue-400">
             <RouterLink to="/about">About</RouterLink>
           </li>
-          <li class="text-sm font-bold text-gray-800 hover:text-blue-400">
+          <li class="text-sm font-bold text-white hover:text-blue-400">
             <RouterLink to="/contact">Contact</RouterLink>
           </li>
         </ul>
