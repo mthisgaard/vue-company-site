e349557a608f67e5fe151d6d2679a1245c4a0b91
Malene Thisgaard <malene.thisgaard@gmail.com>
2023-01-28 13:18:59 +0900

    install and setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/views/**/*.{html,js,vue,ts}",
+    "./src/components/**/*.{html,js,vue,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
